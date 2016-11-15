2cf31059ebf8200b9b1fa971864e2fda3802bd81
ckuh <kuh_chris@yahoo.com>
2016-11-14 19:22:42 -0800

    init setup

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.set('port', process.env.PORT || 1337)
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.listen(app.get('port'), () => console.log('Express server started listening to port ', app.get('port')))
