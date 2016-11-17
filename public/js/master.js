6d62c307e29f5f0e0add9e3c69d7ee158b6109d7
ckuh <kuh_chris@yahoo.com>
2016-11-17 13:19:02 -0800

    added validation

--- public/js/master.js
+++ public/js/master.js
@@ -7,6 +7,7 @@ var machine = $('spin').slotMachine(params)
 var slotMachines = []
 var slotMachinesEl = []
 var slotMachinesTemp = []
+var valid = [false]
 var rolling = true
 
 for(var i = 0; i < 10; i++) {
@@ -20,42 +21,69 @@ for(var i = 0; i < 10; i++) {
   $slotMachine.append(slotMachinesTemp)
 
   slotMachinesEl.push($slotMachine)
-  if(i === 3) { slotMachinesEl.push($('<div class=dash>-</div>')) }
-  if(i === 6) { slotMachinesEl.push($('<div class=dash>-</div>')) }
+  if(i === 2) { slotMachinesEl.push($('<div class=dash>-</div>')) }
+  if(i === 5) { slotMachinesEl.push($('<div class=dash>-</div>')) }
 }
 
 $('document').ready(function() {
   slotMachinesEl.forEach(function(item, index) {
-    $('.content').prepend(item)
+    $('.content').append(item)
     if(item.attr('class') !== 'dash') { slotMachines.push($(item).slotMachine(params)) }
   })
 
   roll()
 
-	$("#yes").click(function(){
+	$('#yes').click(function() {
     if(!rolling) {
-      $oops = $('<div class=oops><h1>Oops</h1></div>')
-      $oops.css({display: 'none'})
-      $('#casino').after($oops)
-      $('.oops').fadeIn('slow')
-      roll()
-      rolling = true
+      if(valid.every(function(item) {return item})) {
+        $oops = $('<div class=oops><h1>Oops</h1></div>')
+        $oops.css({display: 'none'})
+        $('#casino').after($oops)
+        $('.oops').fadeIn('slow')
+        valid = [false]
+        $('.content').children().each(function(item, key) {
+          $(key).toggleClass('correct')
+        })
+        roll()
+        rolling = true
+      } else {
+        $cheat = $('<div class=cheat><h1>Cheater</h1></div>')
+        $cheat.css({display: 'none'})
+        $('#casino').after($cheat)
+        $('.cheat').fadeIn('slow')
+        valid = [false]
+        $('.content').children().each(function(item, key) {
+          if($(key).attr('class').indexOf('correct') !== -1) {
+            $(key).toggleClass('correct')
+          }
+        })
+        roll()
+        rolling = true
+      }
     }
 	})
 
-	$("#no").click(function(){
+	$('#no').click(function() {
     if(!rolling) {
       if($('.oops')) { $('.oops').fadeOut('slow') }
+      if($('.cheat')) { $('.cheat').fadeOut('slow') }
       roll()
-      rolling = true
+      if (!valid.every(function(item) {return item})) {rolling = true}
     }
 	})
+
+  $('.slotMachine').click(function() {
+    $(this).toggleClass('correct')
+    valid[$(this).attr('key')] = !valid[$(this).attr('key')]
+  })
 })
 
 function roll () {
-  slotMachines.forEach(function(item) {
-    item.shuffle(3, function() {
-      rolling = false
-    })
+  slotMachines.forEach(function(item, index) {
+    if(!valid[index]) {
+      item.shuffle(3, function() {
+        rolling = false
+      })
+    }
   })
 }
